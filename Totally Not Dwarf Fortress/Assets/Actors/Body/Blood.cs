4a8b50e9f94564adfe1765a5844c3ba2b9fb874f
Battlespud <battlespud@hotmail.com>
2016-11-12 16:07:24 -0500

    fix

--- Totally Not Dwarf Fortress/Assets/Actors/Body/Blood.cs
+++ Totally Not Dwarf Fortress/Assets/Actors/Body/Blood.cs
@@ -7,7 +7,7 @@ public class Bloodsystem : MonoBehaviour {
 
 	//system for simulating blood flow and loss
 	public double mBlood = 100; //maxblood, in Liters
-	public double cBlood = mBlood; //current blood
+	public double cBlood; //current blood
 	public double pBlood = 1; //blood as a percentage in decimal
 
 	public double regBlood = .02; //regen rate as percentage/hour
