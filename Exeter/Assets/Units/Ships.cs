390cde4207c2cf040c27b7d0fa85b58c2e2efea6
Battlespud <battlespud@hotmail.com>
2016-11-18 01:30:33 -0500

    ftyufty

---
+++ Exeter/Assets/Units/Ships.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ships : MonoBehaviour {
+
+
+	public Fleets.Fleet assignedFleet = Fleets.Fleet.NONE;
+
+	public float movementSpeed = 2;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
